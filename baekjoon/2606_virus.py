a5170e52df66d149e8103c0d42cffc9b8afab101
l-yohai <yohan9612@naver.com>
2021-04-28 15:57:58 +0900

    add baekjoon 2606

---
+++ baekjoon/2606_virus.py
@@ -0,0 +1,23 @@
+n = int(input())
+m = int(input())
+
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x][y] = graph[y][x] = 1
+
+visited = [0] * (n + 1)
+count = 0
+
+
+def dfs(v):
+    visited[v] = 1
+    global count
+    count += 1
+    for i in range(1, len(graph[v])):
+        if visited[i] == 0 and graph[v][i] == 1:
+            dfs(i)
+
+
+dfs(1)
+print(count - 1)

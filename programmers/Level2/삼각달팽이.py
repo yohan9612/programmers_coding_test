4ce8041eb448ce431fb60dd69eb6c64329c82a05
l-yohai <yohan9612@naver.com>
2021-07-01 17:17:06 +0900

    edit: 삼각달팽이

--- programmers/Level2/삼각달팽이.py
+++ programmers/Level2/삼각달팽이.py
@@ -1,19 +1,23 @@
 def solution(n):
-    map = [[0 for i in range(1, j + 1)] for j in range(1, n + 1)]
-    
+    map = [[0 for j in range(i + 1)] for i in range(n)]
+
+    dx = [1, 0, -1]
+    dy = [0, 1, -1]
+
     x, y = -1, 0
     num = 1
-    
+
     for i in range(n):
-        for _ in range(i, n):
-            if i % 3 == 0:
-                x += 1
-            elif i % 3 == 1:
-                y += 1
-            else:
-                x -= 1
-                y -= 1
+        for j in range(i, n):
+            x += dx[i % 3]
+            y += dy[i % 3]
+
             map[x][y] = num
             num += 1
 
     return sum(map, [])
+
+
+print(solution(4))  # [1,2,9,3,10,8,4,5,6,7]
+print(solution(5))  # [1,2,12,3,13,11,4,14,15,10,5,6,7,8,9]
+print(solution(6))  # [1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11]
